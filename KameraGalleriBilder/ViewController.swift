8f37bac4a13b22244e8576c6eee4a27bf41e4fe3
Bill Mårtensson <bill@magictechnology.se>
2020-09-30 13:23:44 +0200

    Initial Commit

---
+++ KameraGalleriBilder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KameraGalleriBilder
+//
+//  Created by Bill Martensson on 2020-09-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
